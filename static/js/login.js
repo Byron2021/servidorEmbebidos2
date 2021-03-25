2a34401cc116b5395597720cf3eb2cc5401bc6a5
Byron2021 <80277101+Byron2021@users.noreply.github.com>
2021-03-25 12:42:11 -0500

    Create login.js

---
+++ static/js/login.js
@@ -0,0 +1,8 @@
+function claveAcceso(){
+	texto=prompt("Introduzca una contraseña");
+		while(texto!="rocklml27"){
+		alert("Contraseña incorrecta");
+			texto=prompt("Introduzca una contraseña");
+		}
+		alert("Contraseña correcta");
+	}
