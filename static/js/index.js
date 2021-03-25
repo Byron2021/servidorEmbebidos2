7ba529d579d66ad43c83b668d4b08ec37b6800f9
Byron2021 <80277101+Byron2021@users.noreply.github.com>
2021-03-25 13:08:11 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -38,3 +38,65 @@ function LED1_Off(){
       }
   
   }
+
+
+// Create a client instance
+  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
+  
+  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
+
+  // set callback handlers
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  var options = {
+   useSSL: false,
+    userName: "byronzeto@gmail.com",
+    password: "rocklml27",
+    onSuccess:onConnect,
+    onFailure:doFail
+  }
+
+  // connect the client
+  client.connect(options);
+   
+  // called when the client connects
+  function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("Conectado...");
+	
+    client.subscribe("byronzeto@gmail.com/test");
+    message = new Paho.MQTT.Message("hola desde la web");
+    message.destinationName = "byronzeto@gmail.com/test1";
+    client.send(message);
+	
+  }
+
+  function doFail(e){
+    console.log(e);
+	
+  }
+
+  // called when the client loses its connection
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+	  document.getElementById("sensor").innerHTML=message.payloadString;
+	  if(message.payloadString===' Encendido'){
+		  document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
+	  } else if(message.payloadString===' Apagado'){
+		  document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
+	  }
+	  if(message.payloadString===' Encendido'){
+		  document.getElementById("btn").innerHTML="Apagar";
+	  }else if(message.payloadString===' Apagado'){
+		  document.getElementById("btn").innerHTML="Encender";
+	  }
+	  
+  }
+  
