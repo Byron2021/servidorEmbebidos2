11cd943a7d9103a26efc32ce8946feb1062f6ef6
Byron2021 <80277101+Byron2021@users.noreply.github.com>
2021-04-12 10:49:12 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,4 @@
-  var btn=document.getElementById('btn'), contador=0;
-  function cambio()
-  { if (contador==0)
-      {
-      message = new Paho.MQTT.Message("ENCENDIDO");
-      message.destinationName = "byronzeto@gmail.com/led1";
-      client.send(message);
-      contador=1;
-      }
-    else
-      {
-      message = new Paho.MQTT.Message("APAGADO");
-      message.destinationName = "byronzeto@gmail.com/led1";
-      client.send(message);
-      contador=0;
-      }
-  
-  }
-
-  function registro()
-      {
-      client.subscribe("byronzeto@gmail.com/led2");
-      message = new Paho.MQTT.Message("HISTORIAL");
-      message.destinationName = "byronzeto@gmail.com/led1";
-      client.send(message);
-      }
-
-
-// Create a client instance
+ // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
@@ -36,8 +8,8 @@
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "byronzeto@gmail.com",
-    password: "rocklml27",
+    userName: "juantixi99@gmail.com",
+    password: "1234",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -50,15 +22,18 @@
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("byronzeto@gmail.com/led");	  
+    //client.subscribe("juantixi99@gmail.com/test");
+    client.subscribe("juantixi99@gmail.com/test1");
+    client.subscribe("juantixi99@gmail.com/test2");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "byronzeto@gmail.com/led1";
+    message.destinationName = "juantixi99@gmail.com/test2";
     client.send(message);
 	
   }
 
   function doFail(e){
-    console.log(e);	
+    console.log(e);
+	
   }
 
   // called when the client loses its connection
@@ -68,21 +43,43 @@
     }
   }
 
-  // called when a message arrives
-var register;
+ // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	if (message.destinationName=="byronzeto@gmail.com/led2"){
-		document.getElementById("cuadro").innerHTML=message.payloadString;
-	  }
-	  if(message.payloadString==='1'){
-		  document.getElementById("sensor").innerHTML=message.payloadString;
-		  document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
-		  document.getElementById("btn").innerHTML="APAGAR";
-	  } else if(message.payloadString==='0'){
-		  document.getElementById("sensor").innerHTML=message.payloadString;
-		  document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
-		  document.getElementById("btn").innerHTML="ENCENDER";
-	  }
+	//document.getElementById("sensor").innerHTML=message.payloadString; 
+	
+//nuevo
+    if(message.destinationName=="juantixi99@gmail.com/test2"){
+    document.getElementById("cuadro").innerHTML=message.payloadString;
+    }
+	if(message.payloadString==='1'){
+		document.getElementById("sensor").innerHTML=message.payloadString;
+		
+		
+	   }
+	if(message.payloadString==='0'){
+		document.getElementById("sensor").innerHTML=message.payloadString;
+	}
 	  
-  }
+}	  
+//fin nuevo
+	//if(message.payloadString==='informacion'){
+	//	document.getElementById("sensor").innerHTML=message.payloadString;	 
+	//} 
+	
+	  
+	  /*
+	  //comando para poner el sensor desde esp32
+	  document.getElementById("sensor").innerHTML=message.payloadString;
+	  if(message.payloadString==='Encendido'){
+		 document.getElementById("imagen").src="http://www.clker.com/cliparts/M/h/R/9/8/H/red-led-on-md.png";
+	  } else if(message.payloadString==='Apagado'){
+		 document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
+ 		
+	  }
+	  if(message.payloadString==='Encendido'){
+	  	document.getElementById("btn").innerHTML="Apagar";
+	  } else if(message.payloadString==='Apagado'){
+		document.getElementById("btn").innerHTML="Encender";
+	  }
+	  */
