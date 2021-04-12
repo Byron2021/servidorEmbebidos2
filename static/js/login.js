caf66499bb28eea052d46853a1e09d029f875d5a
Byron2021 <80277101+Byron2021@users.noreply.github.com>
2021-04-11 22:23:44 -0500

    Delete login.js

--- static/js/login.js
+++
@@ -1,8 +0,0 @@
-function claveAcceso(){
-	texto=prompt("Introduzca una contraseña");
-		while(texto!="rocklml27"){
-		alert("Contraseña incorrecta");
-			texto=prompt("Introduzca una contraseña");
-		}
-		alert("Contraseña correcta");
-	}
